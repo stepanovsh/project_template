e3feef6ebd0f332863849ad87eb64b51e77704e5
Konstantin Moiseenko <moiseenko@steelkiwi.com>
2015-10-28 10:55:36 +0200

    Added vagrantfile.

---
+++ {{cookiecutter.repo_name}}/provision.sh
@@ -0,0 +1,36 @@
+ #!/usr/bin/env bash
+
+if [ -e "/etc/vagrant-provisioned" ];
+then
+    if [ "/vagrant/provision.sh" -ot "/etc/vagrant-provisioned" ];
+    then
+        echo "Vagrant provisioning already completed. Skipping..."
+        exit 0
+    else
+        echo "Starting Vagrant provisioning process..."
+    fi
+
+else
+    echo "Starting Vagrant provisioning process..."
+fi
+
+# Install core components
+/vagrant/sh/core.sh
+
+# Install python
+/vagrant/sh/python.sh
+
+# Install postgresql
+/vagrant/sh/postgresql.sh
+
+# Install redis
+/vagrant/sh/redis.sh
+
+# Install bower
+/vagrant/sh/bower.sh
+
+
+touch /etc/vagrant-provisioned
+
+echo "--------------------------------------------------"
+echo "Your vagrant instance is running"
