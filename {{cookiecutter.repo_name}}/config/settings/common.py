07af7d5b5c2a4d511466ff3124abd548c7d591a3
stepanov <stepanov@steelkiwi.com>
2015-11-15 11:39:11 +0200

    use custom user model rules, fix element

--- {{cookiecutter.repo_name}}/config/settings/common.py
+++ {{cookiecutter.repo_name}}/config/settings/common.py
@@ -223,7 +223,6 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_URL = 'account_login'
-{% else %}
 {%- endif %}
 
 # SLUGLIFIER
